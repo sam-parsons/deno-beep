7ac641012355aa42027f601ab2616fdcf77f65d3
Sam Parsons <samparsons269@gmail.com>
2021-06-27 13:26:47 -0400

    initial commit, first draft

---
+++ deps.ts
@@ -0,0 +1,1 @@
+export { delay } from 'https://deno.land/x/delay@v0.2.0/mod.ts';
---
+++ mod.ts
@@ -0,0 +1,34 @@
+import { delay } from './deps';
+
+function beep() {
+  Deno.stdout.write('\u0007');
+}
+
+export default async function beeper(numberPattern, delayTime = 500) {
+  // handle user supplied flags
+  if (Deno.args.includes('--no-beep')) {
+    return;
+  }
+
+  // handle if first arg is number
+  if (typeof numberPattern === 'number') {
+    for (let i = 0; i < numberPattern; i++) {
+      await delay(delayTime);
+      beep();
+    }
+  } else if (typeof numberPattern === 'string') {
+    const patternArray = numberPattern.split('');
+    if (patternArray[0] === '#') {
+      beep();
+    }
+    delay(delayTime);
+    patternArray.shift();
+    if (patternArray[0]) {
+      beeper(patternArray.join(''), delayTime);
+    } else {
+      return;
+    }
+  } else {
+    beep();
+  }
+}
